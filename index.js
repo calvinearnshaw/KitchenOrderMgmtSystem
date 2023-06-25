8ed71d67264f33deaf198e80327b6855d2d482ae
calvinearnshaw <calvin.earnshaw@outlook.com>
2023-06-25 18:43:55 +0100

    Cleaned up Index.JS - based on template JS file from a University Assignment.

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// NODE.JS Mini Server for Website - CA4
-// Supplied as part of Assignment Setup Instructions
-// Not my own code unless explicitly marked.
-
 const express = require("express");
-const socket = require("socket.io");
 
 // App setup
 const app = express();
@@ -35,38 +30,3 @@ server.listen(6500, () => {
 });
 
 console.log('NOTE: Press Ctrl + C to stop server.');
-
-// Static files
-/*
-app.use(express.static("frontend"));
-
-// Page Not Found Handling
-app.get("/:universalURL", (req, res) => {
-    res.send("404 URL NOT FOUND");
-});
-
-// Socket setup
-const io = socket(server);
-*/
-
-/*io.on("connection", function (socket) {
-    console.log("Made socket connection");
-
-    /*socket.on("new user", function (data) {
-        socket.userId = data;
-
-        activeUsers.add(data);
-        //... is the the spread operator, adds to the set while retaining what was in there already
-        io.emit("new user", [...activeUsers]);
-    });
-
-    socket.on("disconnect", function () {
-        activeUsers.delete(socket.userId);
-        io.emit("user disconnected", socket.userId);
-    });*/
-
-    /*socket.on("new order", function (data) {
-        io.emit("new order", data);
-    });
-
-});*/
